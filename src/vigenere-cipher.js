314f4db7d45f384b2864e431d702b32925bdd06e
BlazarQSO <BlazarQSO@gmail.com>
2020-02-24 18:56:27 +0300

    feat: add 9 and 10 solutions

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,72 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(machine = true) {
+        this.machine = machine;
+        this.alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+        this.tabulaRecta = [];
+        this.tabulaLine = {};
+        this.alphabet.forEach((item, index) => {
+            this.tabulaLine[item] = index;
+            this.tabulaRecta.push([...this.alphabet.slice(index), ...this.alphabet.slice(0, index)]);
+        });
+    }
+    encrypt(message, key) {
+        if (!message || !key) {
+            throw new Error();
+        }
+
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+        let result = '';
+        let indexKey = 0;
+        const keyLength = key.length;
+
+        for (let i = 0; i < message.length; i += 1) {
+            if (this.alphabet.includes(message[i])) {
+                result += this.tabulaRecta[this.tabulaLine[message[i]]][this.tabulaLine[key[indexKey]]];
+                indexKey += 1;
+                if (indexKey === keyLength) {
+                    indexKey = 0;
+                }
+            } else {
+                result += message[i]
+            }
+        }
+        if (this.machine) {
+            return result;
+        } else {
+            return result.split('').reverse().join('');
+        }
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    decrypt(encryptedMessage, key) {
+        if (!encryptedMessage || !key) {
+            throw new Error();
+        }
+
+        let result = '';
+        let indexKey = 0;
+        key = key.toUpperCase();
+        const keyLength = key.length;
+
+        for (let i = 0; i < encryptedMessage.length; i += 1) {
+            if (this.alphabet.includes(encryptedMessage[i])) {
+                const index = this.tabulaRecta.findIndex((item, index) =>
+                    item[this.tabulaLine[key[indexKey]]] === encryptedMessage[i]
+                );
+                result +=  this.tabulaRecta[index][0];
+                indexKey += 1;
+                if (indexKey === keyLength) {
+                    indexKey = 0;
+                }
+            } else {
+                result += encryptedMessage[i];
+            }
+        }
+        if (this.machine) {
+            return result;
+        } else {
+            return result.split('').reverse().join('');
+        }
     }
 }
 
--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,23 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date === undefined) {
+    return 'Unable to determine the time of year!';
+  }
+  if (!(date instanceof Date)) {
+    throw new Error();
+  }
+  if (date.hasOwnProperty('getMonth')) {
+    throw new Error();
+  }
+
+  const month = date.getMonth();
+
+  if (month < 5 && month > 1) {
+    return 'spring';
+  } else if (month > 4 && month < 8) {
+    return 'summer';
+  } else if (month > 7 && month < 11) {
+    return 'fall';
+  } else {
+    return 'winter';
+  }
 };
